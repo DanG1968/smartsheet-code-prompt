aeef4dd9725ddf7a2696c9d7fe44567daf33046b
DanG1968 <dangrin1968@gmail.com>
2024-01-15 18:37:01 +0800

    Second iteration.  Incorporated sort.

--- src/main/java/com/smartsheet/SortSheet.java
+++ src/main/java/com/smartsheet/SortSheet.java
@@ -3,16 +3,14 @@ package com.smartsheet;
 // Add Maven library "com.smartsheet:smartsheet-sdk-java:2.2.3" to access Smartsheet Java SDK
 
 import com.smartsheet.api.Smartsheet;
+import com.smartsheet.api.models.SortSpecifier;
 import com.smartsheet.api.SmartsheetFactory;
+import com.smartsheet.api.models.*;
+import com.smartsheet.api.models.enums.SortDirection;
 import com.smartsheet.api.models.Cell;
 import com.smartsheet.api.models.Column;
-import com.smartsheet.api.models.Row;
-import com.smartsheet.api.models.Sheet;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 
 public class SortSheet {
@@ -31,8 +29,9 @@ public class SortSheet {
             // Initialize client. Gets API access token from SMARTSHEET_ACCESS_TOKEN variable
             Smartsheet smartsheet = SmartsheetFactory.createDefaultClient("C4QWUNdYm67lgrhpTFjbZufc0M8Tm2AEZZujr");
 
-            Sheet sheet = smartsheet.sheetResources().importXlsx("Sample Sheet.xlsx", "sample", 0, 0);
-            //Sheet sheet2 = smartsheet.sheetResources().importCsv ("Sample Sheet.csv", "sample2", 0, 0);
+            Sheet sheet = smartsheet.sheetResources().importCsv ("data.csv", "data", 0, 0);
+
+            int sheetSize = sheet.getRows().size();
 
             // Load the entire sheet
             sheet = smartsheet.sheetResources().getSheet(sheet.getId(), null, null, null, null, null, null, null);
@@ -42,13 +41,35 @@ public class SortSheet {
             for (Column column : sheet.getColumns())
                 columnMap.put(column.getTitle(), column.getId());
 
+            SortCriterion criterion = new SortCriterion();
+            criterion.setColumnId(columnMap.get("country"));
+            criterion.setDirection(SortDirection.ASCENDING);
+            SortCriterion criterion2 = new SortCriterion();
+            criterion2.setColumnId(columnMap.get("state"));
+            criterion2.setDirection(SortDirection.DESCENDING);
+            SortSpecifier specifier = new SortSpecifier();
+            specifier.setSortCriteria(Arrays.asList(criterion, criterion2));
+
+            smartsheet.sheetResources().sortSheet(
+                    sheet.getId(),
+                    specifier
+            );
+
             // Accumulate rows needing update here
-            ArrayList<Row> rowsToUpdate = new ArrayList<Row>();
+            ArrayList<Row> rowsToUpdate = new ArrayList<>();
 
             for (Row row : sheet.getRows()) {
-                Row rowToUpdate = evaluateRowAndBuildUpdates(row);
+
+                Row rowToUpdate = evaluateRowAndBuildUpdates(row, sheetSize);
                 if (rowToUpdate != null)
                     rowsToUpdate.add(rowToUpdate);
+            }
+
+            System.out.println("Sorted");
+
+            int i = 0;
+            for (Row row : rowsToUpdate) {
+                row.setRowNumber(++i);
             }
 
             if (rowsToUpdate.isEmpty()) {
@@ -56,7 +77,7 @@ public class SortSheet {
             } else {
                 // Finally, write all updated cells back to Smartsheet
                 System.out.println("Writing " + rowsToUpdate.size() + " rows back to sheet id " + sheet.getId());
-                smartsheet.sheetResources().rowResources().updateRows(sheet.getId(), rowsToUpdate);
+                //smartsheet.sheetResources().rowResources().updateRows(sheet.getId(), rowsToUpdate);
                 System.out.println("Done");
             }
         } catch (Exception ex) {
@@ -71,27 +92,32 @@ public class SortSheet {
      *
      * Return a new Row with updated cell values, else null to leave unchanged
      */
-    private static Row evaluateRowAndBuildUpdates(Row sourceRow) {
+    private static Row evaluateRowAndBuildUpdates(Row sourceRow, int sheetSize) {
         Row rowToUpdate = null;
 
         // Find cell we want to examine
-        Cell statusCell = getCellByColumnName(sourceRow, "Status");
+        Cell countryCell = getCellByColumnName(sourceRow, "country");
 
-        if ("Complete".equals(statusCell.getDisplayValue())) {
-            Cell remainingCell = getCellByColumnName(sourceRow, "Remaining");
-            if (! "0".equals(remainingCell.getDisplayValue()))                  // Skip if "Remaining" is already zero
-            {
-                System.out.println("Need to update row #" + sourceRow.getRowNumber());
+        if (sourceRow.getRowNumber() < sheetSize) {
+
+            if ("United States".equals(countryCell.getDisplayValue())) {
+
+                Cell stateCell = getCellByColumnName(sourceRow, "state");
+
+                System.out.println("Updating row " + sourceRow.getRowNumber());
+                System.out.println("Current value is " + countryCell.getDisplayValue() + ", " + stateCell.getDisplayValue());
 
                 Cell cellToUpdate = new Cell();
-                cellToUpdate.setColumnId(columnMap.get("Remaining"));
-                cellToUpdate.setValue(0);
+                cellToUpdate.setColumnId(stateCell.getColumnId());
+                cellToUpdate.setValue(stateCell.getDisplayValue().toUpperCase());
 
                 List<Cell> cellsToUpdate = Arrays.asList(cellToUpdate);
 
                 rowToUpdate = new Row();
                 rowToUpdate.setId(sourceRow.getId());
                 rowToUpdate.setCells(cellsToUpdate);
+
+
             }
         }
         return rowToUpdate;
